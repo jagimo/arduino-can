263a977bce0df86b694de3866fb208682976fb9d
jagimo <atelier.f7@gmail.com>
2019-04-27 17:39:36 +0900

    sparkfunのCAMモジュールでread結果をSDに書き込めるように実装

---
+++ src/CAN_Store_SD/CAN_Store_SD.ino
@@ -0,0 +1,76 @@
+//librarys
+#include <Canbus.h>
+#include <defaults.h>
+#include <global.h>
+#include <mcp2515.h>
+#include <mcp2515_defs.h>
+
+//defines
+#define LED0 7
+#define LED1 8
+#define SD_PIN 9
+
+//global value
+File dataFile;
+int fileCount;
+int dataCount;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED0, OUTPUT);
+  pinMode(LED1, OUTPUT);
+
+  //init MCP2515
+  while(1){
+    if(Canbus.init(CANSPEED_500)){  //Initialise MCP2515 CAN controller at the specified speed
+      digitalWrite(LED0, HIGH);
+      delay(1000);
+      break;
+    }else{
+      delay(1000);
+    }
+  }
+
+  //init SD
+  while(1){
+    if(SD.begin(SD_PIN)){
+      digitalWrite(LED1, HIGH);
+      delay(1000);
+      break;
+    }else{
+      delay(1000);
+    }
+  }
+  dataCount = 0
+  fileCount = 0
+}
+
+void loop() {
+  tCAN message;
+  if (mcp2515_check_message()) {
+    if (mcp2515_get_message(&message)){
+      //open new file
+      if(dataCount == 0){
+        dataFile = SD.open("file" + String(fileCount) + ".txt", FILE_WRITE);
+        fileCount++;
+      }
+      dataFile.print("ID: ");
+      dataFile.print(message.id,HEX);
+      dataFile.print(", ");
+      dataFile.print("Data: ");
+      dataFile.print(message.header.length,DEC);
+      for(int i=0;i<message.header.length;i++) { 
+        dataFile.print(message.data[i],HEX);
+        dataFile.print(" ");
+      }
+      dataFile.println("");
+      dataCount++;
+
+      // close file
+      if(dataCount > DATA_SIZE){
+        dataFile.close();
+        dataCount = 0;
+      }  
+    }
+  } 
+}
